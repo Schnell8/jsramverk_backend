681cc787a87603fc5da979c8ac1d590a3965da6d
schnell <christopher.schnell@hotmail.com>
2024-10-20 12:21:17 +0200

    added app.mjs

---
+++ app.mjs
@@ -0,0 +1,42 @@
+import 'dotenv/config';
+
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import documents from "./routes/documents.mjs";
+
+import "./db/mongodb.mjs";
+
+const port = process.env.PORT || 1337;
+
+const app = express();
+
+app.disable('x-powered-by');
+app.set("view engine", "ejs");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// This is middleware called for all routes.
+app.use((req, res, next) => {
+    console.log(req.method);
+    console.log(req.path);
+    next();
+});
+
+// don't show the log when it is test
+if (process.env.NODE_ENV !== 'test') {
+    // use morgan to log at command line
+    app.use(morgan('combined')); // 'combined' outputs the Apache style LOGs
+}
+
+app.use("/documents", documents);
+
+app.get("/", (req, res) => {
+    res.status(200).send('Hello World');
+});
+
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+export default server;
